b813061f98d8f29029788198108eaad20b97e24f
اسمك في GitHub <   GitHub>
2026-05-13 17:43:41 +0300

    create cart model and order models and product controller

---
+++ E-Commerce/Models/Discount.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+
+        [Required, Range(0.01, 100)]
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal DiscountPercentage { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        [NotMapped]
+        public bool IsCurrentlyActive => IsActive && DateTime.UtcNow >= StartDate && DateTime.UtcNow <= EndDate;
+
+
+
+
+
+    }
+}
--- E-Commerce/Models/Review.cs
+++ E-Commerce/Models/Review.cs
@@ -12,6 +12,9 @@ namespace E_Commerce.Models
         public string UserId { get; set; }
         public virtual ApplicationUser User { get; set; }
 
-      
+        public DateTime ReviewDate { get; set; } = DateTime.UtcNow;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
